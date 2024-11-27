4e77b7d853a6752c8935ac65ce8d8d68ac081a08
Arnav Gupta <info@arnavgupta.net>
2024-11-27 19:36:11 +0530

    fix: changed .png with .png.example to avoid dev issues

--- client/app/sitemap.js
+++ client/app/sitemap.js
@@ -1,13 +1,13 @@
 export default async function sitemap() {
   return [
     {
-      url: "https://site.com",
+      url: `${process.env.URL}/path`,
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://site.com/about",
+      url: `${process.env.URL}/path`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
