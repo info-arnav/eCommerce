201d4d9e41177ebb71b373d9875307e498ed13fc
Arnav Gupta <info@arnavgupta.net>
2024-11-28 01:24:02 +0530

    feat: connected database and setup cookies

--- client/app/page.js
+++ client/app/page.js
@@ -1,5 +1,26 @@
+"use client";
+
+import createCookie from "@/components/server/createCookie";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [product, setProducts] = useState("Hi");
+  useEffect(() => {
+    const getProducts = async () => {
+      const data = await fetch("http://localhost:1234/signup", {
+        method: "POST",
+      }).then((e) => {
+        return e.text();
+      });
+      setProducts(data);
+    };
+    getProducts();
+    createCookie("age", "10");
+  }, []);
   return (
-    <div></div>
+    <div>
+      API {product} <br />
+      Hi! the home page of the application is ready.
+    </div>
   );
 }
---
+++ client/components/server/createCookie.js
@@ -0,0 +1,10 @@
+"use server";
+
+import { cookies } from "next/headers";
+export default async function createCookie(key, value) {
+  const cookieStore = await cookies();
+  cookieStore.set(key, value, {
+    httpOnly: true,
+    path: "/",
+  });
+}
